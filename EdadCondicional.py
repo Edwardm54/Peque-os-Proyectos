82849ec4ae5041f0abed5c3711690a28a939e060
Edwardm54 <edward54.m2@gmail.com>
2025-09-17 13:23:31 -0400

    Pruebas minimas

---
+++ EdadCondicional.py
@@ -0,0 +1,17 @@
+print("AQUÍ PUEDE COMPROBAR SU ES MAYOR O MENOR DE EDAD!")
+
+while True:
+
+    edad = int(input("Ingrese su edad: "))
+    print("Su edad es:", edad)
+
+    if edad >= 18:
+        print("Usted es mayor de edad!")
+
+    elif 0 < edad < 18:
+        print("Usted es menor de edad!")
+
+    salir = input("Deseas salir del programa? (s/n): ")
+    if salir == "s":
+        print("Hasta luego!")
+        break
