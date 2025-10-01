7f5ad82954f1e92a72f6fdf7dc34c6db80d39a2f
Edwardm54 <edward54.m2@gmail.com>
2025-10-01 11:47:41 -0400

    Listo

---
+++ SegundosAMinutosHoras.py
@@ -0,0 +1,11 @@
+#De segundos a minutos, horas
+
+segundos = int(input("Ingrese la cantidad de segundos: "))
+
+horas = segundos // 3600
+minutos = (segundos % 3600) // 60
+segundos_restantes = segundos % 60
+
+print(f"{segundos} segundos equivalen a {horas} hora, {minutos} minutos y {segundos_restantes} segundos.")
+
+print("Hasta luego!")
